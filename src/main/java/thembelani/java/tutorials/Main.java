ddc5fba9ceff5559cf7e0d1bab2da55c69d8dade
Thembelani Ngema <thembelaningema20@gmail.com>
2025-10-25 11:25:36 +0200

    Quering data from the database in a generic method for any time of query in any time of view

--- src/main/java/thembelani/java/tutorials/Main.java
+++ src/main/java/thembelani/java/tutorials/Main.java
@@ -57,11 +57,18 @@ public class Main {
 
             System.out.println("=".repeat(50));
 
+            //Setting up column names
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                System.out.printf("%-15s", meta.getColumnName(i).toUpperCase());
+            }
+            System.out.println();
+
             while (resultSet.next()) {
-                System.out.printf("%d - %s %s %n", resultSet.getInt("track_number"),
-                        resultSet.getString("artist_name"),
-                        resultSet.getString("song_title")
-                );
+
+                for (int i = 1; i <= meta.getColumnCount(); i++) {
+                    System.out.printf("%-15s", resultSet.getString(i));
+                }
+                System.out.println();
             }
 
         } catch (SQLException e) {
